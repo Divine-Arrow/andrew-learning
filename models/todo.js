524031527c86c8ae72023ae8a4bcc81a8b7457ab
Bhupender Singh <bhupenders225@gmail.com>
2018-08-21 13:16:25 +0530

    adding todo and user

---
+++ models/todo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+
+    },
+    completed: {
+        type: Boolean,
+        default: true
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+module.exports = {Todo}
