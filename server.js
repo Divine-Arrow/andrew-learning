412fab5fa6dc68c507dd59489e109b4569dba9dd
Bhupender Singh <bhupenders225@gmail.com>
2018-08-22 12:35:46 +0530

    gixing bug 2

--- server.js
+++ server.js
@@ -32,7 +32,22 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     id = req.params.id;
     // isvalid
-    if(!ObjectId.isValid(id)) {
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+      }
+    
+      Todo.findById(id).then((todo) => {
+        if (!todo) {
+          return res.status(404).send();
+        }
+    
+        res.send({todo});
+      }).catch((e) => {
+        res.status(400).send();
+      });
+
+    /* if(!ObjectId.isValid(id)) {
         return res.status(400).send();
     }
 
@@ -42,7 +57,7 @@ app.get('/todos/:id', (req, res) => {
          res.send(result);           
     }).catch((e)=> {
         res.status(404).send();
-    });
+    }); */
 
 });
 
