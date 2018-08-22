1e7a38dea4d268a5c63b98a2c3a03ad77db42a5d
Bhupender Singh <bhupenders225@gmail.com>
2018-08-22 12:50:54 +0530

    fucking bug

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+/* mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
     .then(() => console.log('connection successful'))
-    .catch((err) => console.error(err));;
+    .catch((err) => console.error(err));; */
+
+
+mongoose.connect('process.env.MONGODB_URI', {
+        auth: {
+            user: 'todouser',
+            password: 'mmm&07W7m'
+        }
+    })
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.error(err));
 
 module.exports = {
     mongoose
