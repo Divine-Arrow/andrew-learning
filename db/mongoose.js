19e643ce8ffce3e2a34836a8b35af8be0cbd59ae
Bhupender Singh <bhupenders225@gmail.com>
2018-08-22 23:29:16 +0530

    testing process.env.MONGODB_URI by consoling

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI, {
+    console.log(process.env.MONGODB_URI);
+    /* mongoose.connect(process.env.MONGODB_URI, {
             auth: {
                 user: 'todouser',
                 password: 'mmm&07W7m'
             }
         })
         .then(() => console.log('connection successful'))
-        .catch((err) => console.error(err));
+        .catch((err) => console.error(err)); */
 } else {
     mongoose.connect('mongodb://localhost:27017/TodoApp')
 }
