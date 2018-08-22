c28ba22576ac44d5ccf4cfd00b3b1005fdfcc18a
Bhupender Singh <bhupenders225@gmail.com>
2018-08-22 12:58:38 +0530

    testing something 2

--- db/mongoose.js
+++ db/mongoose.js
@@ -7,8 +7,6 @@ mongoose.Promise = global.Promise;
 
 
 mongoose.connect('process.env.MONGODB_URI')
-    .then(() => console.log('connection successful'))
-    .catch((err) => console.error(err));
 
 module.exports = {
     mongoose
