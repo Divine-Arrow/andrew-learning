43eb6a3c6bc3ce2028e51a409c0c6bef040891e2
Bhupender Singh <bhupenders225@gmail.com>
2018-08-22 23:23:37 +0530

    mongoose server setting for heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-/* mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
-    .then(() => console.log('connection successful'))
-    .catch((err) => console.error(err));; */
 
-
-mongoose.connect('process.env.MONGODB_URI', {
-        auth: {
-            user: 'todouser',
-            password: 'mmm&07W7m'
-        }
-    })
-    .then(() => console.log('connection successful'))
-    .catch((err) => console.error(err));
-
-module.exports = {
-    mongoose
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI, {
+            auth: {
+                user: 'todouser',
+                password: 'mmm&07W7m'
+            }
+        })
+        .then(() => console.log('connection successful'))
+        .catch((err) => console.error(err));
+} else {
+    mongoose.connect('mongodb://localhost:27017/TodoApp')
 }
+
+
+
+module.exports = {mongoose}
